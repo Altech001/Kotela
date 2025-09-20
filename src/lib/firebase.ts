c6d02c7eff3ca067a2255d5887da19ddfb8cd2eb
Abaasa Albert <altechalbert01@gmail.com>
2025-09-20 19:53:27 +0300

    updated

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,14 +6,22 @@ import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+// };
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBq7HfNEYn8AONgsp0omeJyTSHY-bv2FYE",
+  authDomain: "studio-6696156585-bf9e6.firebaseapp.com",
+  projectId: "studio-6696156585-bf9e6",
+  storageBucket: "studio-6696156585-bf9e6.firebasestorage.app",
+  messagingSenderId: "964863360160",
+  appId: "1:964863360160:web:8ac01035fd8ccf7d8eddc5"
 };
 
 // Initialize Firebase
